7edbe760990c5b2db2fed5bd00f504711034a89a
Javier Flores <javiverflo@gmail.com>
2018-06-01 16:57:45 -0400

    Devise auth (#6)
    * add and config devise_token_auth
    
    * fix protect_from_forgery with null_session
    
    * shame rubocop
    
    * add devise_auth gem version, remove duplicated code on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :trackable, :validatable, :confirmable
 end
