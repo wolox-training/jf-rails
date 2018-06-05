6b7914d4ce3b1ca790cc9bf57291f9f4c86adc93
Javier Flores <javier.flores@wolox.cl>
2018-06-05 16:01:20 -0400

    book relation with rents, user relation with books

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
 
   has_many :rents, dependent: :destroy
-  has_many :books, dependent: :destroy
+  has_many :books, through: :rents, dependent: :destroy
 
   # Include devise modules
   devise :database_authenticatable, :registerable, :recoverable,
