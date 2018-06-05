13b37b7f754367d4378e80ac2bcdd071ed305790
Javier Flores <javier.flores@wolox.cl>
2018-06-05 15:53:58 -0400

    continue index and show books services

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,21 @@
 class BooksController < ApplicationController
+  include Wor::Paginate
+  include Brita
+
+  filter_on :author, type: :string
+  filter_on :genre, type: :string
+  filter_on :title, type: :string
+  filter_on :description, type: :string
+
+  # Summary: List books with filters ang paginated
   def index
-    books = Book.all
-    render json: books
+    books = filtrate(Book.all)
+    render_paginated books, each_serializer: BookSerializer
   end
 
-  def show; end
+  # Summary: Show a single book json by id
+  def show
+    return render(plain: 'Not Found', status: 404) unless Book.exists?(params['id'])
+    render json: Book.find(params['id']), serializer: ShowBookSerializer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-
-  get 'books/show'
+  resources :books, only: [:index, :show]
 
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
