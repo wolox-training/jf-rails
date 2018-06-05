0329cbaec380500857114e0a926af7a4ebeefb83
Javier Flores <javier.flores@wolox.cl>
2018-06-05 10:38:46 -0400

    Add user relation with rent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   include DeviseTokenAuth::Concerns::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  has_many :rents
+
+  # Include devise modules
   devise :database_authenticatable, :registerable, :recoverable,
          :rememberable, :trackable, :validatable, :confirmable
 end
