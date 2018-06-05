a6e9e205d949460cac65fa6dc9c579aeacb008f0
Javier Flores <javier.flores@wolox.cl>
2018-06-05 15:53:13 -0400

    add serializers

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,7 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :genre, :author, :image, :title, :publisher, :year
+  attributes :id, :author, :title, :image, :publisher, :year, :genre, :created_at, :updated_at
+
+  def image
+    { url: object.image }
+  end
 end
