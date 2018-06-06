d5832c4fe4b0a58f8663daf0944605c7280b929b
Javier Flores <javier.flores@wolox.cl>
2018-06-06 12:24:17 -0400

    add books_controller tests

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,4 +1,63 @@
 require 'rails_helper'
 
 describe Api::V1::BooksController do
+  include_context 'Authenticated User'
+
+  describe 'GET #index' do
+    context 'When fetching all the books' do
+      let!(:books) { create_list(:book, 3) }
+
+      before do
+        get :index
+      end
+
+      it 'responses with the books json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          books, each_serializer: BookSerializer
+        ).to_json
+
+        expect(JSON.parse(response.body)['page']).to eq(JSON.parse(expected))
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'When show a book' do
+      let!(:book) { create(:book) }
+
+      before do
+        get :show, params: { id: book.id }
+      end
+
+      it 'responses with a book json' do
+        expected = ShowBookSerializer.new(book).to_json
+        expect(JSON.parse(response.body)).to eq(JSON.parse(expected))
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'When show a book rented' do
+      let!(:book) { create(:book_rented) }
+
+      before do
+        get :show, params: { id: book.id }
+      end
+
+      it 'responses with a book json' do
+        expected = ShowBookSerializer.new(book).to_json
+        expect(JSON.parse(response.body)).to eq(JSON.parse(expected))
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end
