82d8940f23604d1d1245682d125eabb13bf10dd8
Javier Flores <javier.flores@wolox.cl>
2018-06-08 14:31:02 -0400

    some fixs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = :sidekiq
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
 
