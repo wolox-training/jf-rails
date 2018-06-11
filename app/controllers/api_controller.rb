28672a9c6372b05e2f54997b573e19bb27e41615
Javier Flores <javiverflo@gmail.com>
2018-06-11 15:42:43 -0400

    add locale translation with i18n to rented email (#15)
    * add locale translation with i18n to rented email
    
    * rubocop private method definition
    
    * newline at end of locale file
    
    * code review

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,11 +8,18 @@ class ApiController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::ParameterMissing, with: :invalid_params
 
+  before_action :set_locale
+
   def record_not_found
     render json: { error: 'Record not found' }, status: :not_found
   end
 
   def invalid_params
     render json: { error: 'Invalid params' }, status: :bad_request
+  end
+
+  # Set locale for translate
+  private def set_locale
+    I18n.locale = current_user.locale || I18n.default_locale
   end
 end
