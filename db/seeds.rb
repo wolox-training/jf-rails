d0ed99d9fcda87edfe07ea3ae3c6e9b986107c3f
Javier Flores <javier.flores@wolox.cl>
2018-06-12 14:22:46 -0400

    resolve schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'test@wolox.com.ar', password: '123123123', password_confirmation: '123123123', first_name: 'Test', last_name: 'TestLastName', confirmed_at: Date.time.now)
+User.create(email: 'javier.flores@wolox.cl', password: '123123123', password_confirmation: '123123123', first_name: 'Javier', last_name: 'Flores', confirmed_at: Date.time.now)
+
+Book.create(genre: "TestGenre", author: "TestAuthor", image: "TestImage", title: "TestTitle", publisher: "TestPublisher", year: "TestYear")
