8fbd0207c1c0bc618b4f173bb2ddacba6fd472d5
Javier Flores <javier.flores@wolox.cl>
2018-06-12 10:41:54 -0400

    user_id not null migration

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20180612141601) do
     t.string "link"
     t.string "publisher"
     t.string "year"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_book_suggestions_on_user_id"
